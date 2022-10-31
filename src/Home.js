08be4e5e06e689b5a36745d76a639feb58e0e54c
Lifguson <lwellsteadgi@gmail.com>
2022-10-31 14:22:58 +0100

    video 22 completed

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,17 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
-    const [name, setName] = useState('mario');
-
-const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-}
-
-useEffect(() => {
-    console.log('use effect ran');
-}, []); /* the second argument of useEffect is a dependency array [] which dictates when it should re-render*/
+ /* the second argument of useEffect is a dependency array [] which dictates when it should re-render*/
 
 return ( 
     <div className="home">
-        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+        {error && <div>{error}</div>}
+        {isPending && <div>Loading...</div>}
+        {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" /> */}
     </div>
     );
