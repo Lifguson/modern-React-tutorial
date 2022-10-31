65c01a63d9eaec4db51bd52cfce01b8fcb6c571b
Lifguson <lwellsteadgi@gmail.com>
2022-10-31 12:55:30 +0100

    video 16 completed

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
 
-    const handleClick = () => {
-        setName('luigi');
-        setAge(30);
-    }
+const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+}
 
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
-        </div>
-     );
+useEffect(() => {
+    console.log('use effect ran');
+}, []); /* the second argument of useEffect is a dependency array [] which dictates when it should re-render*/
+
+return ( 
+    <div className="home">
+        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+        {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" /> */}
+    </div>
+    );
 }
  
 export default Home;
